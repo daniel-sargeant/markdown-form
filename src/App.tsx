e9371fb0cf1ffdd1fb79b8f90b0f76b38a038e7f
Daniel Sargeant <daniel.sargeant@thebaltimorebanner.com>
2025-10-25 11:56:33 -0400

    add submit and create

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, type FormEventHandler } from "react";
 import "./App.css";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
@@ -13,6 +13,7 @@ interface Post {
 function App() {
   const [markdownState, setMarkDownState] = useState("");
   const [posts, setPosts] = useState<Post[] | null>(null);
+  const [currentPost, setCurrentPost] = useState<Post | null>(null);
 
   useEffect(() => {
     const getPosts = () => fetch("http://localhost:3000/");
@@ -21,32 +22,70 @@ function App() {
       .then((p) => setPosts(p));
   }, []);
 
+  const submitPost = async (body: string) =>
+    await fetch("http://localhost:3000/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body,
+    });
+
   return (
     <>
       <h1>Only Exhausting Art</h1>
       <div className="card">
         <p>Existing Posts</p>
-        <button disabled>Create New Post</button>
+        <div
+          style={{ display: "flex", flexDirection: "column", width: "500px" }}
+        >
+          {posts?.map((p) => (
+            <button
+              onClick={() => {
+                setCurrentPost(p);
+                setMarkDownState(p.content!);
+              }}
+            >
+              {p.id} – {p.created_at}
+            </button>
+          ))}
+        </div>
+        <button
+          onClick={() => {
+            setCurrentPost(null);
+            setMarkDownState("");
+          }}
+        >
+          New Post
+        </button>
+        <button
+          form={currentPost ? currentPost.id : "new"}
+          type="submit"
+          disabled={!markdownState.length}
+          onClick={() =>
+            submitPost(
+              JSON.stringify({
+                id: currentPost ? currentPost.id : null,
+                content: markdownState,
+              })
+            )
+          }
+        >
+          Submit Post
+        </button>
       </div>
       <div className="content">
-        <form>
+        <form name={currentPost ? currentPost.id : "new"}>
           <textarea
             onInput={(e) =>
               setMarkDownState((e.target as HTMLTextAreaElement).value)
             }
+            value={markdownState}
           ></textarea>
         </form>
         <div className="preview">
           <Markdown remarkPlugins={[remarkGfm]}>{markdownState}</Markdown>
         </div>
-      </div>
-      {console.log(posts)}
-      <div>
-        {posts?.map((p) => (
-          <p>
-            {p.id} – {p.created_at}
-          </p>
-        ))}
       </div>
     </>
   );
